afa5a8ea71ad67ffa64ff4dd72d2e3859bc66e86
Sergi <garretaserra@gmail.com>
2019-11-23 12:02:32 +0100

    added scripts to run the configuration

---
+++ dockerscript.sh
@@ -0,0 +1,1 @@
+sudo docker run -it -p6633:6633 -p8080:8080 -p6653:6653 lvillatoroq/odl:hydrogen
---
+++ script.sh
@@ -0,0 +1,1 @@
+sudo mn --controller=remote,ip=127.0.0.1,port=6633 --mac --custom netScript.py --topo myTopo --switch ovsk
